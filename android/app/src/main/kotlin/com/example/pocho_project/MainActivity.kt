3b67dde536faab04762e5034b64805224c3d2600
Zain <syedzainnaqvi3324@gmail.com>
2022-05-31 22:28:16 +0500

    firebase firestore firestorage connectivity with flutter

---
+++ android/app/src/main/kotlin/com/example/pocho_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pocho_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:pocho_project/home/home.dart';
+import 'package:pocho_project/splash/splash.dart';
+
+void main() async {
+  // initializing the firebase firestore
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: "AIzaSyB6dcXHX0umJAvPEToSwfyQgYzaozi8V0A",
+        appId: "1:259485677318:web:c1809b0492c18760151087",
+        messagingSenderId: "259485677318",
+        projectId: "instagram-58391",
+        storageBucket: "instagram-58391.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
